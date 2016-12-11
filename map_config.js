aa8b27bdafad40ab3b1246d69913d32dfae63899
Supashape <pascal.suhm@gmail.com>
2016-12-11 17:56:19 +0100

    zoom to  mid of Germany, fixed zoomlevel

--- map_config.js
+++ map_config.js
@@ -2,11 +2,13 @@
 //Set fixed map frame
 //zoom to middle of Germany
 
-     var map = L.map('map').setView([-41.2858, 174.78682], 14);
+     var map = L.map('map').setView([51.199635, 10.452953], 6);
+			
         mapLink = 
             '<a href="http://openstreetmap.org">OpenStreetMap</a>';
         L.tileLayer(
             'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; ' + mapLink + ' Contributors',
-            maxZoom: 18,
+            minZoom: 6,
+			maxZoom: 18,
             }).addTo(map);
